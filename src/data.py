7f87be40828972f74723899cf92932fb8100be0e
SurajDonthi <surajdonthi95@gmail.com>
2020-12-04 19:14:26 +0530

    Updated code in engine.py and data.py

--- src/data.py
+++ src/data.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentParser
 import pytorch_lightning as pl
 from torch.utils.data import Dataset, DataLoader
 
@@ -18,10 +17,11 @@ class Dataset(Dataset):
 
 class CustomDataLoader(pl.LightningDataModule):
 
-    def __init__(self, data_dir,
-                 train_batchsize=32,
-                 val_batchsize=32,
-                 test_batchsize=32,
+    def __init__(self, data_dir: str,
+                 train_batchsize: int = 32,
+                 val_batchsize: int = 32,
+                 test_batchsize: int = 32,
+                 num_workers: int = 4,
                  train_transforms=None,
                  val_transforms=None,
                  test_transforms=None):
@@ -30,9 +30,17 @@ class CustomDataLoader(pl.LightningDataModule):
 
         self.data_dir = data_dir
 
+        if not self.data_dir.exists():
+            raise Exception(
+                f"'Path '{self.data_dir.__str__()}' does not exist!")
+        if not self.data_dir.is_dir():
+            raise Exception(
+                f"Path '{self.data_dir.__str__()}' is not a directory!")
+
         self.train_batchsize = train_batchsize
         self.test_batchsize = test_batchsize
         self.val_batchsize = val_batchsize
+        self.num_workers = num_workers
 
         self.train_transforms = train_transforms
         self.val_transforms = val_transforms
@@ -46,18 +54,15 @@ class CustomDataLoader(pl.LightningDataModule):
 
     def train_dataloader(self):
         dataset = Dataset()
-        # ToDo: Remove hardcoding
         return DataLoader(dataset, batch_size=self.train_batchsize,
-                          shuffle=True, num_workers=4)
+                          shuffle=True, num_workers=self.num_workers)
 
     def val_dataloader(self):
         dataset = Dataset()
-        # ToDo: Remove hardcoding
         return DataLoader(dataset, batch_size=self.train_batchsize,
-                          shuffle=True, num_workers=4)
+                          shuffle=True, num_workers=self.num_workers)
 
     def test_dataloader(self):
         dataset = Dataset()
-        # ToDo: Remove hardcoding
         return DataLoader(dataset, batch_size=self.train_batchsize,
-                          shuffle=True, num_workers=4)
+                          shuffle=True, num_workers=self.num_workers)
--- src/engine.py
+++ src/engine.py
@@ -3,6 +3,12 @@ import pytorch_lightning as pl
 
 from .model import Model
 
+LOSSES = {'bce': F.binary_cross_entropy,
+          'bce_logits': F.binary_cross_entropy_with_logits,
+          'cross_entropy': F.cross_entropy, 'nll_loss': F.nll_loss,
+          'kl_div': F.kl_div, 'mse': F.mse_loss,
+          'l1_loss': F.l1_loss}
+
 
 class Engine(Model, pl.LightningModule):
 
@@ -12,8 +18,15 @@ class Engine(Model, pl.LightningModule):
     @staticmethod
     def add_model_specific_args(cls, parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser.add_argument('-des', '--description', required=False, type=str)
+        parser.add_argument('-lp', '--log_path', type=str,
+                            default='./lightning_logs')
+        parser.add_argument('-lr', '--learning_rate',
+                            type=float, default=0.1)
+        parser.add_argument('-c', '--criterion', type=str,
+                            choices=LOSSES.keys(),
+                            default='cross_entropy')
         parser.add_argument()
-
         return parser
 
     def configure_optimizers(self):
