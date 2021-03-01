9a5b6b035abd6ee00d6596fbbdb64ee9224baa36
SurajDonthi <surajdonthi95@gmail.com>
2021-03-01 12:32:03 +0000

    Updated tuner. Possible bugs.🐞

--- src/tuner.py
+++ src/tuner.py
@@ -3,10 +3,16 @@ from argparse import Namespace
 
 args = Namespace()
 
-args.description = "Training with Amazon outdoors data"
+args.description = "Training with Amazon outdoors data for Sentiment Analysis"
+args.task = "sentiment-analysis"
 args.log_path = "./logs"
-args.max_epochs = 20
+args.max_epochs = 1
 args.data_path = "./data/amazon_outdoors/amazon_reviews_us_Outdoors_v1_00_rebalanced.csv.gz"
+args.data_args = dict(max_len=512,
+                      read_args=dict(nrows=7500,
+                                     usecols=['review_body', 'sentiment'])
+                      )
+args.lr = 1e-3
 args.train_split_ratio = 0.7
 # args.limit_train_batches = 0.01
 # args.limit_val_batches = 0.01
@@ -15,4 +21,4 @@ args.train_batchsize = 16
 args.val_batchsize = 16
 args.test_batchsize = 16
 args.debug = False
-args.git_tag = True
+args.git_tag = False
