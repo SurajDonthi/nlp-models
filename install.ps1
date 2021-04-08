ad70badc0ab4a37330399bd0a28f42779a2e5a53
SurajDonthi <surajdonthi95@gmail.com>
2021-04-08 18:58:18 +0530

    Fixed bugs. Updated Environment

--- install.ps1
+++ install.ps1
@@ -1,4 +1,6 @@
+echo "Cleaning up older environment"
 conda env remove -n NLPDev
+echo "Creating environment NLPDev..."
 conda env create -f environment.yml
 conda activate NLPDev
 echo "Activated NLPDev"
@@ -7,10 +9,11 @@ python -m spacy download en_core_web_md
 python -m spacy download en_core_web_sm
 python -m spacy download en
 echo "Installing neuralcoref..."
-git clone https://github.com/huggingface/neuralcoref.git ~/neuralcoref
-Push-Location ~/neuralcoref/
+git clone https://github.com/huggingface/neuralcoref.git $HOME/neuralcoref
+Push-Location $HOME/neuralcoref/
 pip install -r requirements.txt
 pip install -e .
 Pop-Location
 # echo "Deleting neuralcoref folder..."
 # rm -r ./neuralcoref
+conda clean -ay
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo "Cleaning up older environment"
+conda env remove -n NLPDev
 conda env create -f environment.yml
 eval "$(conda shell.bash hook)"
 conda activate NLPDev
@@ -14,11 +16,4 @@ pip install -e .
 cd -
 # echo "Deleting neuralcoref folder..."
 # rm -r ./neuralcoref
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O drivers/google-chrome-stable_current_amd64.deb
-sudo dpkg -i drivers/google-chrome-stable_current_amd64.deb
-wget https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_linux64.zip -O drivers/chromedriver_linux64.zip
-unzip drivers/chromedriver_linux64.zip -d drivers/
-mv drivers/chromedriver /usr/bin/chromedriver
-chown root:root /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
+conda clean -ay
